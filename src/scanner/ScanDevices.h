d98b672d23c89c6ebbac9e0719a70894a8343fba
SmonSE <68323384+SmonSE@users.noreply.github.com>
2026-03-18 14:27:12 +0100

    Update ScanDevices.h
    fix wrong include path

--- src/scanner/ScanDevices.h
+++ src/scanner/ScanDevices.h
@@ -2,12 +2,12 @@
 
 #include <Arduino.h>
 #include "../config/config.h"
-#include "../gattServices/batteryLevelService.h"
-#include "../gattServices/deviceInfoService.h"
-#include "../gattServices/heartRateService.h"
-#include "../gattServices/temperatureService.h"
-#include "../gattServices/genericAccessService.h"
-#include "../gattServices/currentTimeService.h"
+#include "../GATTServices/batteryLevelService.h"
+#include "../GATTServices/deviceInfoService.h"
+#include "../GATTServices/heartRateService.h"
+#include "../GATTServices/temperatureService.h"
+#include "../GATTServices/genericAccessService.h"
+#include "../GATTServices/currentTimeService.h"
 #include "../target/TargetDevice.h"
 
 #include <NimBLEDevice.h>
